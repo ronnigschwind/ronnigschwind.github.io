28ff6ca757468c2b9dd70c241d56b0378c44b232
Ronni Hartlage <hartlage.7@wright.edu>
2022-03-01 00:27:25 -0500

    BrowserRouter changed to HashRouter so that links can be made between pages on GitHub

--- ronnihartlage-reactapp/src/App.js
+++ ronnihartlage-reactapp/src/App.js
@@ -1,4 +1,5 @@
-import { Route, BrowserRouter as Router, Routes } from 'react-router-dom';
+// BrowserRouter -> HashRouter
+import { Route, HashRouter as Router, Routes, Link } from 'react-router-dom';
 import './App.css';
 import Header from './components/Header';
 import Footer from './components/Footer';
@@ -11,17 +12,18 @@ import Contact from './pages/Contact';
 function App() {
   return (
     <div className="App">
-      <Header />
-      {/* other possible sections: education, experience */}
       <Router>
+        <Header />
+      {/* other possible sections: education, experience */}
+      {/* <Router> */}
         <Routes>
             <Route exact path="/" element={<Home/>}/>
             <Route path="/projects" element={<Projects/>}/>
             <Route path="/about" element={<About/>}/>
             <Route path="/contact" element={<Contact/>}/>
         </Routes>
+        <Footer />
       </Router>
-      <Footer />
     </div>
   );
 }
--- ronnihartlage-reactapp/src/components/Header.js
+++ ronnihartlage-reactapp/src/components/Header.js
@@ -1,4 +1,8 @@
+import React from 'react';
 import '../styles/Header.css';
+
+// maybe?
+import { Link, HashRouter as Router } from 'react-router-dom';
 
 // nav bar and seciton above
 function Header() {
@@ -6,14 +10,14 @@ function Header() {
     <div className="Header">
       <div className="headerLinks">
           <h2 style={{ display: "inline-block", marginRight: "100px", color: "white", height: "200px"}}>Ronni Hartlage</h2>
-          <a href="/" style={{ margin: "10px" }}>Home </a>
-          <a href="/about" style={{ margin: "10px" }}>About </a>
-          <a href="/projects" style={{ margin: "10px" }}>Projects </a>
-          <a href="/contact" style={{ margin: "10px" }}>Contact </a>
+          <Link to="/" style={{ margin: "10px" }}>Home</Link>
+          <Link to="/about" style={{ margin: "10px" }}>About</Link>
+          <Link to="/projects" style={{ margin: "10px" }}>Projects</Link>
+          <Link to="/contact" style={{ margin: "10px" }}>Contact</Link>
       </div>
       <h1 style={{ position: "absolute", width: "100%", top: "100px", color: "white" }}>Welcome to the Website of Ronni Hartlage!</h1>
       <h2 style={{ position: "absolute", width: "100%", top: "150px", color: "white" }}>This site is a work in progress, but will eventually be a portfolio for Ronni Hartlage.</h2>
-      <a href="/contact" style={{ color: "white", padding: "10px", borderRadius: "8px", backgroundColor: "black" }}>Contact Ronni</a>
+      <Link to="/contact" style={{ color: "white", padding: "10px", borderRadius: "8px", backgroundColor: "black" }}>Contact Ronni</Link>
     </div>
   );
 }
