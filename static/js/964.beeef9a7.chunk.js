9b7e4b22d73adec72ec4dc2daab8a08ad7c9db80
ronnigschwind <ronnigschwind@users.noreply.github.com>
2022-12-17 17:59:15 +0000

    deploy: 3676e53fb38303e734a24340e6d04f79989e335a

---
+++ static/js/964.beeef9a7.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkronnigschwind_reactapp=self.webpackChunkronnigschwind_reactapp||[]).push([[964],{964:function(n,e,r){r.r(e);var s=r(184);e.default=function(){return(0,s.jsx)("div",{className:"About",children:(0,s.jsx)("p",{children:"About page in progress..."})})}}}]);
+//# sourceMappingURL=964.beeef9a7.chunk.js.map
