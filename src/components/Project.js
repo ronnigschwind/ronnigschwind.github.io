c4d4ff8cafb10b11518950ccebf8b5712ca10afb
Ronni Hartlage <hartlage.7@wright.edu>
2023-03-02 22:41:09 -0500

    Adding projects to Project page

---
+++ src/components/Project.js
@@ -0,0 +1,16 @@
+import '../styles/Hero.css';
+
+function Project(props) {
+  return (
+    <div className='Project' style={{display: 'flex', gap: 50, alignItems: 'center', padding: '0 20px 50px', maxWidth: 1200, margin: 'auto'}}>
+        <a href={props.link} target="_blank" rel="noreferrer"><img src={props.img} style={{maxWidth: 600, boxShadow: '10px 10px 15px #888888'}} alt='Website Project'/></a>
+        <div>
+          <h2>{props.title}</h2>
+          <p style={{paddingBottom: 20}}>{props.description}</p>
+          <a href={props.link} className='glowOnHover' target="_blank" rel="noreferrer" style={{ color: "white", background: "black", padding: "5px", borderRadius: "8px" }}>Check Out the Project</a>
+        </div>
+    </div>
+  )
+}
+    
+export default Project
--- src/pages/Projects.js
+++ src/pages/Projects.js
@@ -1,16 +1,78 @@
-import Banner from "../components/Banner";
+import Project from '../components/Project';
+import artUnlimited from '../images/art-unlimited.webp'
+import victors from '../images/victors.webp'
+import irelandContracting from '../images/ireland-contracting.webp'
+import roofPro from '../images/roof-pro.webp'
+import arrowheadLibrary from '../images/arrowhead-library.webp'
+import gorman from '../images/gorman-roofing.webp'
+import reliant from '../images/reliant.webp'
+import whiteWilderness from '../images/white-wilderness.webp'
+import goodNeighbors from '../images/good-neighbors.webp'
+
+const projectsList = [
+  {
+    img: artUnlimited,
+    title: 'Art Unlimited React Web App',
+    description: 'Redid the previous Art Unlimited website that was done previously in WordPress in React with an entirely new design.',
+    link: 'https://www.artunlimitedusa.com/',
+  },
+  {
+    img: victors,
+    title: 'Victors Home Solutions React Web App',
+    description: 'Redid the previous Victors Home Solutions website that was done previously in WordPress in React with an entirely new design.',
+    link: 'https://www.victors.com/',
+  },
+  {
+    img: irelandContracting,
+    title: 'Ireland Contracting WordPress Website',
+    description: 'Created a new website using WordPress for Ireland Contracting.',
+    link: 'https://www.irelandcontracting.com/',
+  },
+  {
+    img: roofPro,
+    title: 'RoofPro WordPress Website',
+    description: 'Created a new website using WordPress used for marketing to roofing companies.',
+    link: 'https://roofpro.artunlimitedusa.com/',
+  },
+  {
+    img: arrowheadLibrary,
+    title: 'Arrowhead Library WordPress Website',
+    description: 'Created a new website using WordPress for Arrowhead Library.',
+    link: 'https://www.alslib.info/',
+  },
+  {
+    img: gorman,
+    title: 'Gorman Roofing WordPress Website',
+    description: 'Created a new website using WordPress for Gorman Roofing.',
+    link: 'https://www.gormanroofing.com/',
+  },
+  {
+    img: reliant,
+    title: 'Reliant Roofing WordPress Website',
+    description: 'Created a new website using WordPress for Reliant Roofing.',
+    link: 'https://www.reliantroofing.com/',
+  },
+  {
+    img: whiteWilderness,
+    title: 'White Wilderness WordPress Website',
+    description: 'Created a new website using WordPress for White Wilderness.',
+    link: 'https://www.whitewilderness.com/',
+  },
+  {
+    img: goodNeighbors,
+    title: 'Good Neighbors EMS WordPress Website',
+    description: 'Created a new website using WordPress for Good Neighbors EMS.',
+    link: 'https://www.goodneighborsems.com/',
+  }
+]
 
 function Projects() {
     return (
       <div className="Projects">
-        <Banner/>
-
-        {/* <iframe src='https://www.goodneighborsems.com/' height='300' width='100%' title='Good Neighbors EMS Website'></iframe> */}
-
-        {/* ideas: - links to sites I've made
-                   - list projects I've done using a varitey
-                   - future project ideas / projects in progress
-        */}
+        <h1 style={{textAlign: 'center', padding: '50px 20px', fontSize: 50, maxWidth: 600, margin: 'auto'}}>Check Out Some of My Latest Projects!</h1>
+        {projectsList.map((project, index) => (
+          <Project key={index} img={project.img} title={project.title} description={project.description} link={project.link}/>
+        ))}
       </div>         
     );
   }
