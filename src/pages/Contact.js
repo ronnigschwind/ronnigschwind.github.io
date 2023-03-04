38b42936e4b5b260e304fafd0c257800b6ef2a13
Ronni Hartlage <hartlage.7@wright.edu>
2023-03-03 23:12:33 -0500

    Removing unused code and reformatting for consistency on Contact page

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -1,17 +1,15 @@
-import Banner from "../components/Banner";
-
 function Contact() {
-    return (
-      <div className="Contact" style={{textAlign: 'center', maxWidth: 800, margin: 'auto', padding: '0 20px 50px'}}>
-        <h1 style={{textAlign: 'center', padding: '50px 20px', fontSize: 50, margin: 'auto'}}>Contact Me For Career or Personal Project Opportunities</h1>
-        <p>
-          I am currently looking for a new career opportunity and would love to connect if you are in need of a full-time, contract, or freelance developer. Feel free to 
-          reach out by email or connect with me on LinkedIn and I'd love to chat. Thank you for your interest in me!
-        </p>
-        <p style={{ display: "inline-block", paddingRight: "3px"}}>Email: <a href="mailto:ronnigschwind@gmail.com<">ronnigschwind@gmail.com</a></p>
-        <p>LinkedIn: <a href="https://www.linkedin.com/in/ronni-gschwind-0a7199177/" target="_blank" rel='noreferrer'>https://www.linkedin.com/in/ronni-gschwind-0a7199177/</a></p>
-      </div>
-    );
-  }
+  return (
+    <div className="Contact" style={{textAlign: 'center', maxWidth: 800, margin: 'auto', padding: '0 20px 50px'}}>
+      <h1 style={{textAlign: 'center', padding: '50px 20px', fontSize: 50, margin: 'auto'}}>Contact Me For Career or Personal Project Opportunities</h1>
+      <p>
+        I am currently looking for a new career opportunity and would love to connect if you are in need of a full-time, contract, or freelance developer. Feel free to 
+        reach out by email or connect with me on LinkedIn and I'd love to chat. Thank you for your interest in me!
+      </p>
+      <p style={{ display: "inline-block", paddingRight: "3px"}}>Email: <a href="mailto:ronnigschwind@gmail.com<">ronnigschwind@gmail.com</a></p>
+      <p>LinkedIn: <a href="https://www.linkedin.com/in/ronni-gschwind-0a7199177/" target="_blank" rel='noreferrer'>https://www.linkedin.com/in/ronni-gschwind-0a7199177/</a></p>
+    </div>
+  );
+}
   
   export default Contact;
